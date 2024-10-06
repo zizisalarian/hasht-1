981e5b33bb5a9f40a86119b6eb5ebfa18daf593a
@zeynabsalarian <zizisalarian@gmail.com>
2024-10-06 10:41:19 +0330

    add line in mobile mode

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ let advantageCards = document.querySelector('.advantages-cards')
 let advantageCard = document.querySelector('.advantages-card')
 let advantageNext = document.querySelector('.advantages-nav.next')
 let advantagePrev = document.querySelector('.advantages-nav.prev')
+if(advantageCard){
 
-let cardWidth = advantageCard.getBoundingClientRect().width
-
-advantageNext.addEventListener('click' , ()=>{
+    let cardWidth = advantageCard.getBoundingClientRect().width
+    
+    advantageNext.addEventListener('click' , ()=>{
     let scrollLeftA = advantageCards.scrollLeft
     console.log('scrollLeftA' , scrollLeftA , scrollLeftA+cardWidth )
     advantageCards.scrollTo(scrollLeftA+cardWidth  , 0 )
@@ -16,6 +17,7 @@ advantagePrev.addEventListener('click' , ()=>{
     advantageCards.scrollTo(scrollLeftA - cardWidth  , 0 )
     
 })
+}
 
 
 
@@ -23,33 +25,38 @@ let usercommentCards = document.querySelector('.user-comment-container')
 let usercommentCard = document.querySelector('.user-comment')
 let usercommentNext = document.querySelector('.user-comments .next')
 let usercommentPrev = document.querySelector('.user-comments .prev')
-let cardWidthComment = usercommentCard.getBoundingClientRect().width
-usercommentNext.addEventListener('click' , ()=>{
-    let scrollLeftA = usercommentCards.scrollLeft
-    console.log('scrollLeftA' , scrollLeftA , scrollLeftA+cardWidthComment )
-    usercommentCards.scrollTo(scrollLeftA+cardWidthComment  , 0 )
-})
+if(usercommentCard){
+
+    let cardWidthComment = usercommentCard.getBoundingClientRect().width
+    usercommentNext.addEventListener('click' , ()=>{
+        let scrollLeftA = usercommentCards.scrollLeft
+        console.log('scrollLeftA' , scrollLeftA , scrollLeftA+cardWidthComment )
+        usercommentCards.scrollTo(scrollLeftA+cardWidthComment  , 0 )
+    })
 usercommentPrev.addEventListener('click' , ()=>{
     let scrollLeftA = usercommentCards.scrollLeft
     console.log('scrollLeftA' , scrollLeftA , scrollLeftA-cardWidthComment )
     usercommentCards.scrollTo(scrollLeftA - cardWidthComment  , 0 )
     
 })
+}
 
 let newsCards = document.querySelector('.news-cards')
 let newsCard = document.querySelector('.news-card')
 let newsNext = document.querySelector('.last-news .next')
 let newsPrev = document.querySelector('.last-news .prev')
-let cardWidthNews = newsCard.getBoundingClientRect().width
-console.log(cardWidthNews)
-newsNext.addEventListener('click' , ()=>{
-    let scrollLeftA = newsCards.scrollLeft
-    console.log('scrollLeftA' , scrollLeftA , scrollLeftA+cardWidthNews )
-    newsCards.scrollTo(scrollLeftA+cardWidthNews  , 0 )
-})
+if(newsCard){
+    let cardWidthNews = newsCard.getBoundingClientRect().width
+    console.log(cardWidthNews)
+    newsNext.addEventListener('click' , ()=>{
+        let scrollLeftA = newsCards.scrollLeft
+        console.log('scrollLeftA' , scrollLeftA , scrollLeftA+cardWidthNews )
+        newsCards.scrollTo(scrollLeftA+cardWidthNews  , 0 )
+    })
 newsPrev.addEventListener('click' , ()=>{
     let scrollLeftA = newsCards.scrollLeft
     console.log('scrollLeftA' , scrollLeftA , scrollLeftA-cardWidthNews )
     newsCards.scrollTo(scrollLeftA-cardWidthNews , 0 )
     
 })
+}
